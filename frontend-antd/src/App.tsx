7c0e862d35055c470d164c7288041e7d23d8d8bd
İsmail BAŞARAN <basaran.ismaill@gmail.com>
2022-07-23 22:43:40 +0300

    antd version

---
+++ frontend-antd/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormEditor from './components/FormEditor/FormEditor';
+//import FormEditorBasic from './components/FormEditorBasic/FormEditorBasic';
+
+const App = () => {
+  return (
+    <FormEditor />
+  )
+}
+
+export default App;
