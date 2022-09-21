66975a70566222338dfb86a880e68a546d1c3557
sercan-basaran <sercanbasaaran@gmail.com>
2022-09-21 09:25:51 +0300

    Language Changed

--- frontend/src/components/FormEditorBasic/FormEditorBasic.tsx
+++ frontend/src/components/FormEditorBasic/FormEditorBasic.tsx
@@ -39,14 +39,8 @@ const RootBehavior = createBehavior({
     droppable: true,
   },
   designerLocales: {
-    'zh-CN': {
-      title: '根组件',
-    },
     'en-US': {
       title: 'Root',
-    },
-    'ko-KR': {
-      title: '루트',
     },
   },
 })
@@ -82,16 +76,16 @@ const InputBehavior = createBehavior({
             },
             test: {
               type: 'void',
-              title: '测试',
+              title: 'test',
               'x-decorator': 'FormItem',
               'x-component': 'DrawerSetter',
               'x-component-props': {
-                text: '打开抽屉',
+                text: 'Open the drawer',
               },
               properties: {
                 test: {
                   type: 'string',
-                  title: '测试输入',
+                  title: 'Test input',
                   'x-decorator': 'FormItem',
                   'x-component': 'Input',
                 },
@@ -99,11 +93,10 @@ const InputBehavior = createBehavior({
             },
           },
         },
-        
 
         'component-styles': {
           type: 'void',
-          title: '样式',
+          title: 'style',
           'x-component': 'CollapseItem',
           properties: {
             'style.width': {
@@ -146,26 +139,6 @@ const InputBehavior = createBehavior({
     },
   },
   designerLocales: {
-    'zh-CN': {
-      title: '输入框',
-      settings: {
-        title: '标题',
-        hidden: '是否隐藏',
-        default: '默认值',
-        style: {
-          width: '宽度',
-          height: '高度',
-          display: '展示',
-          background: '背景',
-          boxShadow: '阴影',
-          font: '字体',
-          margin: '外边距',
-          padding: '内边距',
-          borderRadius: '圆角',
-          border: '边框',
-        },
-      },
-    },
     'en-US': {
       title: 'Input',
       settings: {
@@ -183,26 +156,6 @@ const InputBehavior = createBehavior({
           padding: 'Padding',
           borderRadius: 'Border Radius',
           border: 'Border',
-        },
-      },
-    },
-    'ko-KR': {
-      title: '입력',
-      settings: {
-        title: '텍스트',
-        hidden: '숨김 여부',
-        default: '기본 설정 값',
-        style: {
-          width: '너비',
-          height: '높이',
-          display: '디스플레이',
-          background: '배경',
-          boxShadow: '그림자 박스',
-          font: '폰트',
-          margin: '마진',
-          padding: '패딩',
-          borderRadius: '테두리 굴곡',
-          border: '테두리',
         },
       },
     },
@@ -283,14 +236,8 @@ const CardBehavior = createBehavior({
     },
   },
   designerLocales: {
-    'zh-CN': {
-      title: '卡片',
-    },
     'en-US': {
       title: 'Card',
-    },
-    'ko-KR': {
-      title: '카드',
     },
   },
 })
@@ -299,16 +246,14 @@ GlobalRegistry.setDesignerBehaviors([RootBehavior, InputBehavior, CardBehavior])
 
 const Input = createResource({
   title: {
-    'zh-CN': '输入框',
     'en-US': 'Input',
-    'ko-KR': '입력 상자',
   },
   icon: 'InputSource',
   elements: [
     {
       componentName: 'Field',
       props: {
-        title: '输入框',
+        title: 'Input box',
         type: 'string',
         'x-decorator': 'FormItem',
         'x-component': 'Input',
@@ -319,29 +264,20 @@ const Input = createResource({
 
 const Card = createResource({
   title: {
-    'zh-CN': '卡片',
     'en-US': 'Card',
-    'ko-KR': '카드 상자',
   },
   icon: 'CardSource',
   elements: [
     {
       componentName: 'Card',
       props: {
-        title: '卡片',
+        title: 'card',
       },
     },
   ],
 })
 
 GlobalRegistry.registerDesignerLocales({
-  'zh-CN': {
-    sources: {
-      Inputs: '输入控件',
-      Displays: '展示控件',
-      Feedbacks: '反馈控件',
-    },
-  },
   'en-US': {
     sources: {
       Inputs: 'Inputs',
@@ -349,26 +285,19 @@ GlobalRegistry.registerDesignerLocales({
       Feedbacks: 'Feedbacks',
     },
   },
-  'ko-KR': {
-    sources: {
-      Inputs: '입력',
-      Displays: '디스플레이',
-      Feedbacks: '피드백',
-    },
-  },
 })
 
 const Logo: React.FC = () => (
   <div style={{ display: 'flex', alignItems: 'center', fontSize: 14 }}>
     <IconWidget
-      infer="Logo"
+      infer='Logo'
       style={{ margin: 10, height: 24, width: 'auto' }}
     />
   </div>
 )
 
 const Actions = observer(() => {
-  const supportLocales = ['zh-cn', 'en-us', 'ko-kr']
+  const supportLocales = ['en-us']
   useEffect(() => {
     if (!supportLocales.includes(GlobalRegistry.getDesignerLanguage())) {
       GlobalRegistry.setDesignerLanguage('zh-cn')
@@ -379,22 +308,21 @@ const Actions = observer(() => {
     <Space style={{ marginRight: 10 }}>
       <Radio.Group
         value={GlobalRegistry.getDesignerLanguage()}
-        optionType="button"
+        optionType='button'
         options={[
           { label: 'English', value: 'en-us' },
-          { label: '简体中文', value: 'zh-cn' },
-          { label: '한국어', value: 'ko-kr' },
+          { label: 'Türkçe', value: 'tr-tr' },
         ]}
         onChange={(e) => {
           GlobalRegistry.setDesignerLanguage(e.target.value)
         }}
       />
-      <Button href="https://github.com/alibaba/designable" target="_blank">
+      <Button href='https://github.com/alibaba/designable' target='_blank'>
         <GithubOutlined />
         Github
       </Button>
-      <Button>保存</Button>
-      <Button type="primary">SAVE</Button>
+      <Button>save</Button>
+      <Button type='primary'>SAVE</Button>
     </Space>
   )
 })
@@ -406,22 +334,21 @@ const FormEditorBasic = () => {
       <Workbench>
         <StudioPanel logo={<Logo />} actions={<Actions />}>
           <CompositePanel>
-            <CompositePanel.Item title="panels.Component" icon="Component">
-              <ResourceWidget title="sources.Inputs" sources={[Input, Card]} />
+            <CompositePanel.Item title='panels.Component' icon='Component'>
+              <ResourceWidget title='sources.Inputs' sources={[Input, Card]} />
               <ResourceWidget
-                title="sources.Displays"
+                title='sources.Displays'
                 sources={[Input, Card]}
               />
               <ResourceWidget
-                title="sources.Feedbacks"
+                title='sources.Feedbacks'
                 sources={[Input, Card]}
               />
-              
             </CompositePanel.Item>
-            <CompositePanel.Item title="panels.OutlinedTree" icon="Outline">
+            <CompositePanel.Item title='panels.OutlinedTree' icon='Outline'>
               <OutlineTreeWidget />
             </CompositePanel.Item>
-            <CompositePanel.Item title="panels.History" icon="History">
+            <CompositePanel.Item title='panels.History' icon='History'>
               <HistoryWidget />
             </CompositePanel.Item>
           </CompositePanel>
@@ -431,14 +358,14 @@ const FormEditorBasic = () => {
               <ViewToolsWidget />{' '}
             </ToolbarPanel>
             <ViewportPanel>
-              <ViewPanel type="DESIGNABLE">{() => <Content />}</ViewPanel>
-              <ViewPanel type="JSONTREE">
+              <ViewPanel type='DESIGNABLE'>{() => <Content />}</ViewPanel>
+              <ViewPanel type='JSONTREE'>
                 {() => {
                   return (
                     <div style={{ overflow: 'hidden', height: '100%' }}>
                       <MonacoInput
-                        language="javascript"
-                        helpCode="//hello world"
+                        language='javascript'
+                        helpCode='//hello world'
                         defaultValue={`<div><div>123123<div>123123<div>123123<div>123123</div></div></div></div></div>`}
                       />
                     </div>
@@ -447,8 +374,8 @@ const FormEditorBasic = () => {
               </ViewPanel>
             </ViewportPanel>
           </WorkspacePanel>
-          <SettingsPanel title="panels.PropertySettings">
-            <SettingsForm uploadAction="https://www.mocky.io/v2/5cc8019d300000980a055e76" />
+          <SettingsPanel title='panels.PropertySettings'>
+            <SettingsForm />
           </SettingsPanel>
         </StudioPanel>
       </Workbench>
@@ -456,4 +383,4 @@ const FormEditorBasic = () => {
   )
 }
 
-export default FormEditorBasic;
+export default FormEditorBasic
